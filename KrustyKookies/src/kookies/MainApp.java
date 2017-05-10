4a5105586c72e5ad1fdba531237987a64c99d02b
simma <simma@DESKTOP-TVDAK3O.lan>
2017-05-10 20:05:00 +0200

    latest

--- KrustyKookies/src/kookies/MainApp.java
+++ KrustyKookies/src/kookies/MainApp.java
@@ -1,19 +1,27 @@
 package kookies;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import kookies.model.Cookie;
 import kookies.model.Database;
+import kookies.model.Ingredient;
 
 public class MainApp extends Application {
 	
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private ObservableList<Cookie> cookieType = FXCollections.observableArrayList();//temp
+	private ArrayList<Ingredient> list = new ArrayList<Ingredient>();//temp
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -46,21 +54,42 @@ public class MainApp extends Application {
 		}
 	}
 	
-	
+	public MainApp(){//temp
+		
+		cookieType.add(new Cookie("Nut Ring"));
+		cookieType.add(new Cookie("Nut Cookie"));
+		cookieType.add(new Cookie("Tango"));
+		cookieType.add(new Cookie("Amneris"));
+		cookieType.add(new Cookie("Almond Delight"));
+		cookieType.add(new Cookie("Berliner"));
+		
+		
+	}
+	public ObservableList<Cookie> getCookieType(){//temp
+		return cookieType;
+	}
 	public void showProductionScreen(){
 		try{
 			//FXMLLoader loader = new FXMLLoader();
 			//loader.setLocation(MainApp.class.getResource("view/ProductionScreen.fxml"));
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("view/ProductionScreen.fxml"));
+
 			AnchorPane productionScreen = (AnchorPane) loader.load();
 			
 			rootLayout.setCenter(productionScreen);
+			
+			//Give the controller access to the main app
+			ProductionController controller = loader.getController();
+			controller.setMainApp(this);
+			
 			
 		}catch(IOException e){
 			System.out.println("PRODUCTION SCREEN EXCEPTION");
 			//e.printStackTrace();
 		}
 	}
+	
+	
 	
 	
 	public Stage getPrimaryStage() {
--- KrustyKookies/src/kookies/ProductionController.java
+++ KrustyKookies/src/kookies/ProductionController.java
@@ -1,7 +1,42 @@
 package kookies;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import kookies.model.Cookie;
+
 public class ProductionController {
 	
+	@FXML
+	private Button add;
+	@FXML
+	private Button produce;	
+	@FXML
+	private ListView<String> ingredients;
+	@FXML
+	private ListView<String> cookies;
+	@FXML
+	private ListView<String> recieps;
+	@FXML
+	private TextField text;
 	
+	private MainApp mainApp;
+	
+	public ProductionController(){
+		
+	}
+	
+	@FXML
+	private void initialize(){
+		//lambdauttryck? sätt igång alla listviews 
+		
+	}
+	
+	public void setMainApp(MainApp mainApp){
+		
+		this.mainApp = mainApp;
+	}
 
 }
